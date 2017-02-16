e3d3b6a3f4bee970a31dd7c012d6ffcbe5550fbb
Dante Regis <dante@danteregis.com>
2017-02-15 16:08:46 -0800

    Failing test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "will fail. why?" do
+    @mock = MiniTest::Mock.new
+    @mock.expect(:world, "test world")
+
+    User.stub(:find, @mock) do
+      get user_path(id: 1, format: :hal_json)
+      json = JSON.parse(response.body)
+      assert_equal "test world", json["hello"]
+    end
+  end
 end
